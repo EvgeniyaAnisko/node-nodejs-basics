5250b4135b9f4418cdc37258d237524b27a0e680
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2023-06-19 15:50:22 +0300

    feat: add fs/rename resolve

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import { rename as renameFile, access, constants } from 'node:fs/promises';
+import { getAbsolutePath } from './getAbsolutePath.js';
+
+const FILE_NAME = 'wrongFilename.txt';
+const FILE_NAME_NEW = 'properFilename.md';
+const ERROR_MESSAGE = 'FS operation failed';
+
 const rename = async () => {
-    // Write your code here 
+    const urlOldFile = getAbsolutePath(FILE_NAME);
+    const urlNewFile = getAbsolutePath(FILE_NAME_NEW);
+
+    try {
+        await access(urlNewFile, !constants.F_OK);
+    } catch {
+        try {
+            await renameFile(urlOldFile, urlNewFile);
+        } catch {
+            throw new Error(ERROR_MESSAGE);
+        }
+    }
+
 };
 
 await rename();
