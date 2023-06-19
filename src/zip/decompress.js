745dcc88b800f84740ab7d1eb2d0a0fa50721100
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2023-06-19 20:51:57 +0300

    feat: add zip/decompress resolve

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { pipeline } from "node:stream/promises";
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGunzip } from "node:zlib";
+import { fileURLToPath } from "url";
+import path from "node:path";
+
+const FILE_NAME = "files/fileToCompress.txt";
+const ARCHIVE_NAME = "files/archive.gz";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const url = path.join(__dirname, FILE_NAME);
+    const urlArchive = path.join(__dirname, ARCHIVE_NAME);
+
+    await pipeline(createReadStream(urlArchive), createGunzip(), createWriteStream(url));
 };
 
 await decompress();
