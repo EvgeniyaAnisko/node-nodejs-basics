6f1c932e4a5e721c8176e47c16be2fc860898a8f
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2023-06-19 14:42:14 +0300

    feat: add fs/copy resolve

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,27 @@
+import { copyFile, mkdir, readdir } from 'fs/promises';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const DIRICTORY_ORIGIN_NAME = 'files';
+const DIRICTORY_COPY_NAME = 'files_copy';
+const ERROR_MESSAGE = 'FS operation failed';
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        const [files] = await Promise.all([readdir(path.join(__dirname, DIRICTORY_ORIGIN_NAME)), mkdir(path.join(__dirname, DIRICTORY_COPY_NAME))])
+        const promises = files.map(fileName => {
+            copyFile(path.join(__dirname, DIRICTORY_ORIGIN_NAME, fileName), path.join(__dirname, DIRICTORY_COPY_NAME, fileName));
+        });
+        await Promise.all(promises)
+    } catch (error) {
+        throw new Error(ERROR_MESSAGE);
+    }
 };
 
 await copy();
