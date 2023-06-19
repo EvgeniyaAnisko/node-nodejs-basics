3bbe434615d9e2f55935127112f0ac85388cb31a
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2023-06-19 20:02:42 +0300

    feat: add streams/read resolve

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { pipeline } from 'stream/promises';
+
+const FILE_NAME = 'files/fileToRead.txt';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const url = path.join(__dirname, FILE_NAME);
+    await pipeline(createReadStream(url), process.stdout);
 };
 
 await read();
