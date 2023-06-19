f45120ec8e529bb29f09bcd35452171cab829f72
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2023-06-19 15:19:39 +0300

    feat: add fs/delete resolve

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { unlink } from 'node:fs/promises';
+import { getAbsolutePath } from './getAbsolutePath.js';
+
+const FILE_NAME = 'fileToRemove.txt';
+const ERROR_MESSAGE = 'FS operation failed';
+
 const remove = async () => {
-    // Write your code here 
+    const url = getAbsolutePath(FILE_NAME)
+    try {
+        await unlink(url)
+    } catch (err) {
+        throw new Error(ERROR_MESSAGE);
+    }
+    
 };
 
 await remove();
