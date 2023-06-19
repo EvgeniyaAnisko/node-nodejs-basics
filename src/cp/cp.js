df7619217ecc579d29a571311ed6636c1838cd5e
EvgeniyaAnisko <evgeniya.anisko@gmail.com>
2023-06-19 23:00:02 +0300

    feat: add cp resolve

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { fileURLToPath } from "url";
+import path from "node:path";
+import { fork } from "child_process";
+
+const CHILD_FILE = "files/script.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const urlChild = path.join(__dirname, CHILD_FILE);
+
+    fork(urlChild, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', '...']);
